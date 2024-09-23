65b59e6314e5c7d39d0dbfaf4f23ce8eac7be11d
cocoman99 <cocoman99@o2.pl>
2024-09-23 22:14:02 +0200

    Helo 2

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use ferris_says::say;
 use std::io::{stdout, BufWriter};
+use std::io;
 
 fn main() {
     let stdout = stdout();
@@ -9,4 +10,7 @@ fn main() {
     let mut writer = BufWriter::new(stdout.lock());
     say(&message, width , &mut writer).unwrap();
     //fsays --width 12 ("Hello fellow Rustaceans!");
+
+    println!("{}", "\nPress ENTER".to_string());
+    let _ = io::stdin().read_line(&mut String::new());
 }
