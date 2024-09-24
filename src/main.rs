664c49f3856f5cf517469a469c5021b1539da5d3
cocoman99 <cocoman99@o2.pl>
2024-09-24 21:51:38 +0200

    Helo 3
    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
-use ferris_says::say;
-use std::io::{stdout, BufWriter};
-use std::io;
+extern crate ferris_says;
+
+//use ferris_says::say;
+use std::io::{stdout, BufWriter, self};
 
 fn main() {
-    let stdout = stdout();
-    let message = String::from("Hello fellow CoCo !");
-    let width = message.chars().count();
-
-    let mut writer = BufWriter::new(stdout.lock());
-    say(&message, width , &mut writer).unwrap();
-    //fsays --width 12 ("Hello fellow Rustaceans!");
-
-    println!("{}", "\nPress ENTER".to_string());
-    let _ = io::stdin().read_line(&mut String::new());
+    let mut count = 0;
+        while count == 0 {  
+            let out = "Hello fellow CoCo !";
+            let width = 12;   
+            let mut writer = BufWriter::new(stdout());
+            ferris_says::say(out, width, &mut writer).unwrap();
+            count = 1;
+        }
+    println!("\nPRESS ENTER");
+    io::stdin().read_line(&mut String::new()).unwrap();
 }
